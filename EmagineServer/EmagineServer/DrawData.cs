7932fa5ab2419ed70ec4db033bb7f31892e7b914
gobind.johar <gobind.johar@c4322efe-5a93-f492-c35e-5959b38fda22>
2010-03-29 17:05:58 +0000

    checking in emagine server code rev 1

---
+++ EmagineServer/EmagineServer/DrawData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Channels;
+
+namespace EmagineServer
+{
+    [MessageContract]
+    public class DrawData
+    {
+        public const string DrawAction = "http://amazedsaint.net/SilverPaint/draw";
+
+        [MessageBodyMember]
+        public string Content { get; set; }
+
+        [MessageBodyMember]
+        public string From { get; set; }
+    }
+}
